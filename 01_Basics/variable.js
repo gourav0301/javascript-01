6df313a15e761c82a8b9cec404d6859179e833a9
gourav0301 <149614820+gourav0301@users.noreply.github.com>
2023-12-10 18:03:36 +0000

    Container Config Done

---
+++ 01_Basics/variable.js
@@ -0,0 +1,16 @@
+var name="Gaurav";
+name='Jangra';
+console.log(name);
+// In var we redclared the value as well as we update the value also
+//  and we can access the value of var in all over the code.
+// Global Scope
+
+let firstName="Jangra";
+firstName="Gaurav";
+console.log(firstName);// Gaurav
+// let having the block level scope means within block we can access the value.
+// we can't redecalre the value of let
+
+const lastName="Geetansh";
+// lastName="Dev"; we can'nt do this in case of const and const having blocked scope
+console.log(lastName);
